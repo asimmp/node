34712c902c74fdb97bc0ba699029f973973e2b39
ASIM MP <161556228+asimmp@users.noreply.github.com>
2024-12-09 14:31:20 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,3 +1,36 @@
+const express= require ('express')
+
+const mysql= require('mysql')
+
+const hbs= require('hbs')
+
+const app=express();
+
+var db = mysql.createConnection({
+
+host:"localhost",
+
+user:"root",
+
+password:"",
+database:"nodepro"
+
+})
+
+db.connect((err)=>{
+
+if(!err)
+
+{
+
+console.log("Connected")
+
+}
+
+else{
+
+console.log("Connection Failed")}})
+
 
 const path= require ('path');
 //connect viewengine
@@ -5,4 +38,5 @@ app.set('views', path.join( dirname));
 app.set('view engine','hbs')
 app.get('/',(req,res)=>{
 res.render('views/firstpage')
+})
 app.listen(8080)
